d1f5ae9e35fbd7c514ea1497c97608265e70d32d
PaPaPaGod <inteli5760@163.com>
2020-03-13 12:34:51 +0800

    v1.0 gmm爬虫号更新，完成账号信息爬取，对符合条件的号自动发送邮件到邮箱

--- excel/sendemail.py
+++ excel/sendemail.py
@@ -10,13 +10,6 @@ import json
 
 # fake_ua = UserAgent()
 
-
-url = 'http://www.gmmsj.com/dy/89_zh.shtml'
-
-area_url = 'http://www.gmmsj.com/gatew/CfgGW/getGameAreaGroupList?app_version=1.0.0.53327&device_id=K4lW2WaCuP0eYzY69THfpdCZxiGkuA0e&system_deviceId=K4lW2WaCuP0eYzY69THfpdCZxiGkuA0e&app_channel=chrome&src_code=7&game_id=89&type=area&count=1000'
-base_goods_url = 'http://www.gmmsj.com/dy/89_zh/detail_{}.shtml'
-
-
 class Mail:
     def __init__(self):
         self.mail_host = "smtp.qq.com"
@@ -25,8 +18,7 @@ class Mail:
         self.receivers = ['352619255@qq.com']
 
 
-    def send(self):
-        content = 'dn'
+    def send(self,content):
         message = MIMEText(content,'plain','utf-8')
         message['From'] = Header("cao",'utf-8')
         message['To'] = Header('heh','utf-8')
@@ -43,29 +35,74 @@ class Mail:
 
 
 class GMM:
-    def __init__(self):
-        self.page = '1'
-        self.p_job = '48'
+    def __init__(self,page,p_job,price,area_id):
+        left = '[\"'
+        right = '\"]'
+        self.page = page
+        self.p_job = left+p_job+right
+        self.price = left+price[0]+'\",\"'+price[1]+right
+        self.area_id = area_id
         self.base_url = 'http://www.gmmsj.com/gatew/gmmGoodsGW/goodsList'
+        self.base_good_url = 'http://www.gmmsj.com/dy/89_zh/detail_{0}.shtml'
+        self.base_good_detail_url = 'http://www.gmmsj.com/gmminf/accountapi/goods/'
+        self.params = {
+            "book_id":''
+        }
+        self.good_data = {
+            'src_code':7,
+            'method':'GetGoodsStatus',
+            'params':self.params,
+        }
 
+    # "price": "[\"{p1}}\",\"{p2}\"]"
 
-
+    def dispose(self,content):
+        content = json.loads(content)
+        list = content['data']
+        goodList = list['goodsList']
+        flag = False
+        if(len(goodList)):
+            for item in goodList:
+                title = item['goods_list_title']
+                price = item['price']+'元'
+                book_id = item['book_id']
+                self.params['book_id'] = book_id
+                params = json.dumps(self.params)
+                self.good_data['params'] = params
+                # res1 = requests.get(self.base_good_detail_url, params=self.good_data)
+                url = self.base_good_url.format(book_id)
+                mail = Mail()
+                response = requests.get(url)
+                html = etree.HTML(response.text)
+                i = 1
+                content = 'No.'+ str(i) +'\t' +title +'\t'+price
+                # content = 'No.'+ str(i) +'\t' +title +'\t'+price
+                path = html.xpath('string(//div[@class="tab-content"]/div[@class="tab-pane active detail"]/section[@class="desc"])')
+                if title.find('变换')>=0 and (path.find('头')>=0 or path.find('脚')>=0) :
+                    path = path.replace(' ', '')
+                    path = path.replace('\t', '')
+                    path.strip('\n')
+                    content = content+path + url
+                    print(content)
+                    # mail.send(content)
+                # mail.send(content)
 
 if __name__ == '__main__':
-    gmm = GMM()
+    gmm = GMM(1,'24',['5001','10000'],2)
     # 搜索参数设置
     properties = {
-        'p_job':gmm.p_job
+        'p_job':gmm.p_job,
+        'price':gmm.price
     }
     properties = json.dumps(properties)
-    p_new = properties.replace(' ','')
+    # p_new = properties.replace(' ','')
     # 区服参数调协
     area_id = {
         'area_id':gmm.area_id
     }
     area_id_list = [area_id]
     area_id_groups = json.dumps(area_id_list)
-    area_id_groups = area_id_groups.replace(' ','')
+    # area_id_groups = area_id_groups.replace(' ','')
     header ={
         'user-agent':'Mozilla/5.0 (Windows NT 6.1; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/80.0.3987.132 Safari/537.36',
         'Host':'www.gmmsj.com',
@@ -73,7 +110,7 @@ if __name__ == '__main__':
     }
     # 设置传参
     data = {
-        'searchProperties':p_new,
+        'searchProperties':properties,
         'src_code':7,
         'order_type':'modify_time',
         'order_dir':'d',
@@ -82,4 +119,6 @@ if __name__ == '__main__':
         'area_id_groups':area_id_groups
     }
     response = requests.get(gmm.base_url,params=data)
-    print(response.url)
+    print(response.text)
+    gmm.dispose(response.text)
+
